28e60770dccf962cc195dcb500452c6c6a22c4e2
sqrt <sqrt@octoblu.com>
2016-07-05 15:23:19 -0700

    This seems to actually work...

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MeshbluVerifierSocket.IO.NET
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
